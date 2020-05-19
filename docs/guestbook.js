5b617144518443bb8705e267d989a3dfcf0242a0
Mohamed Belghiti <Mohamed.Belghiti@us.ibm.com>
2020-05-19 14:00:52 -0400

    add api url

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://d52c80b3.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
